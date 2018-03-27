7b067702bbbdcdcb824e0695fed1795961c782bb
Nick Carrasco <nickc321@gmail.com>
2018-03-26 20:14:32 -0400

    initial file typing

---
+++ config.py
@@ -0,0 +1,5 @@
+#! /usr/bin/env python
+
+## Module for parsing configuration file 
+## @ author: N Carrasco
+
---
+++ wallpaper.py
@@ -0,0 +1,5 @@
+#! /usr/bin/env python
+
+## Module to set desktop wallpaper based on window manager
+## @ author: N Carrasco
+
