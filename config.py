823b5533e6c1c1ab741e36633a40acac78a6b588
ckcin@github.com <nickc321@gmail.com>
2018-04-10 00:19:35 -0400

    initial setup of configuration options..values currently ignored by imagery.py

--- config.py
+++ config.py
@@ -6,4 +6,21 @@
 import configargparse
 
 def buildParser():
-    parser = configargparse.ArgParser(default_config_files=['/etc/app/conf.d/*.conf', './bluearth.conf'])
+    parser = configargparse.ArgParser(default_config_files=['/etc/app/conf.d/*.conf', './bluearth.conf'],
+                                      formatter_class=configargparse.ArgumentDefaultsHelpFormatter)
+    parser.add('-c', '--config', is_config_file=True, help="path to local/override config file")
+    parser.add('--url', help="url for retriving image",
+               default=None)
+    parser.add('--image', help="ALIAS for predefined image urls",
+               choices=['CONUS','GOES_EAST','GOES_WEST','GLOBAL'],
+               default='CONUS')
+    parser.add('--refresh', help="Refresh rate (in minutes) for retreiving images in daemon mode",
+            default=15)
+    parser.add('--daemon', '-d', help="Run as daemon",
+               action='store_true',
+               default=False)
+    parser.add('-v', '--verbose', help="verbose mode",
+               action='store_true',
+               default=False)
+
+    return parser
