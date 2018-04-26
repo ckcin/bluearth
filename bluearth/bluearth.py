bc7bc4aa85b3dc0507ae488e4dddc305359477e5
ckcin@github.com <nickc321@gmail.com>
2018-04-26 10:13:30 -0400

    added basic logging

--- bluearth/bluearth.py
+++ bluearth/bluearth.py
@@ -9,15 +9,23 @@ class bluearthd():
     def __init__(self):
         print "do something daemony"
 
+def log(level=False,msg=None):
+    if level and msg is not None:
+        print msg
+
 ###########################
 if __name__ == "__main__":
-    print "bluearth" 
     config = config.buildParser().parse_args()
-    print config
+    log(config.verbose, config)
+
+    log(config.verbose,"retrieving image from "+(config.url or imagery.imagery_urls[config.image]))
 
     # get imagery
     image = imagery.getImage(url=config.url or imagery.imagery_urls[config.image],
                              folder=config.storage)
 
+    log(config.verbose,"image retrieved. written to: "+image)
+
     # set wallpaper
+    log(config.verbose,"setting wallpaper")
     wallpaper.set_wallpaper(image)
